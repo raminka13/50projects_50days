fc1387d93bc07abf767c8e648046cd00b15bee1e
Raul Ospina <raminka13@hotmail.com>
2023-02-24 11:10:50 -0500

    Change script.js

--- button-ripple-effect/script.js
+++ button-ripple-effect/script.js
@@ -1,7 +1,8 @@
 const buttons = document.querySelectorAll('.ripple');
 
 buttons.forEach((button) => {
-  button.addEventListener('click', (e) => {
+  // eslint-disable-next-line func-names
+  button.addEventListener('click', function (e) {
     const x = e.pageX;
     const y = e.pageY;
 
