ecc24772fa71d2fc64fe93aa208db1fa05d6bd2c
Raul Ospina <raminka13@hotmail.com>
2023-03-16 15:42:05 -0500

    Update Styles

--- expanding_cards/script.js
+++ expanding_cards/script.js
@@ -1,4 +1,23 @@
 const panels = document.querySelectorAll('.panel');
+const panelImages = document.querySelectorAll('.panel');
+
+const imagesArray = [
+  'https://images.unsplash.com/photo-1558979158-65a1eaa08691?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1572276596237-5db2c3e16c5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1353&q=80',
+  'https://images.unsplash.com/photo-1551009175-8a68da93d5f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+  'https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+];
+
+const addImage = (panel, image) => {
+  panel.style.backgroundImage = `url(${image})`;
+};
+
+const addImages = () => {
+  panelImages.forEach((panel, index) => {
+    addImage(panel, imagesArray[index]);
+  });
+};
 
 function removeActiveClasses() {
   panels.forEach((panel) => {
@@ -12,3 +31,5 @@ panels.forEach((panel) => {
     panel.classList.add('active');
   });
 });
+
+addImages();
