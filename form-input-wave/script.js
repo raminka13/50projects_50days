67e91c335890655adb5b2138a7351bc508aaa366
Raul Ospina <raminka13@hotmail.com>
2023-03-17 09:38:30 -0500

    Update style

--- form-input-wave/script.js
+++ form-input-wave/script.js
@@ -1,8 +1,13 @@
 const labels = document.querySelectorAll('.form-control label');
+const container = document.querySelector('.container');
 
 labels.forEach((label) => {
   label.innerHTML = label.innerText
     .split('')
     .map((letter, idx) => `<span style="transition-delay:${idx * 20}ms">${letter}</span>`)
     .join('');
+});
+
+window.addEventListener('load', () => {
+  container.classList.add('loaded');
 });
