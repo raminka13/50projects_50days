c0f14375150b16d677c39040b329e54ad08e52f2
Raul Ospina <raminka13@hotmail.com>
2023-04-05 11:23:30 -0500

    Update Styles

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -1,19 +1,53 @@
-const boxes = document.querySelectorAll('.box');
+const container = document.querySelector('.container');
+let lastCard = container.lastElementChild;
+let boxes = [];
 
-function checkBoxes() {
-  const triggerBottom = (window.innerHeight / 5) * 4;
+const getRandomColor = (s) => {
+  const h = Math.floor(Math.random() * 255);
 
-  boxes.forEach((box) => {
-    const boxTop = box.getBoundingClientRect().top;
+  return `radial-gradient(hsl(${h}deg, ${s + 40 * 2}%, 70%),hsl(${h}deg, ${s + 40 * 2}%, 60%))`;
+};
 
-    if (boxTop < triggerBottom) {
-      box.classList.add('show');
-    } else {
-      box.classList.remove('show');
+const populateCards = (num) => {
+  for (let i = 0; i < num; i += 1) {
+    const div = document.createElement('div');
+    const p = document.createElement('h2');
+
+    div.classList.add('box');
+    div.style.background = getRandomColor(i);
+    p.textContent = 'Content';
+    div.appendChild(p);
+    container.appendChild(div);
+  }
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const intersecting = entry.isIntersecting;
+    if (intersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target);
+      boxes = document.querySelectorAll('.box');
+      boxes.forEach((box) => {
+        observer.observe(box);
+      });
+      observer.unobserve(entry.target);
     }
+    if (entry.target === lastCard && intersecting) {
+      populateCards(3);
+      observer.unobserve(lastCard);
+      lastCard = document.querySelector('.container').lastElementChild;
+      observer.observe(lastCard);
+    }
+    // if (!intersecting) {
+    //   entry.target.classList.remove('show');
+    // }
   });
-}
+});
 
-window.addEventListener('scroll', checkBoxes);
-
-checkBoxes();
+populateCards(4);
+boxes = document.querySelectorAll('.box');
+boxes.forEach((box) => {
+  observer.observe(box);
+});
+observer.observe(lastCard);
